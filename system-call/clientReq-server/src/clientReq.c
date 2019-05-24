9253a8aaba9d8fbcf241b0adde0f227c53dcd6d3
AlexPasqua <alexpasquali98@gmail.com>
2019-05-24 23:06:02 +0200

    inserimento servizio con funz apposita per controllo stringhe

--- system-call/clientReq-server/src/clientReq.c
+++ system-call/clientReq-server/src/clientReq.c
@@ -1,28 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+// funz per controllare l'inserimento del servizio richiesto
+bool check_service_input(char *service){
+  char *avail_servs[] = {"stampa", "salva", "invia"};
+
+  for (int i = 0; i < 3; i++)
+    if (strcmp(service, avail_servs[i]) == 0)
+      return true;
+
+  printf("\nErr. Il nome va scelto tra \"stampa\" - \"salva\" - \"invia\"\n");
+  return false;
+}
 
 int main (int argc, char *argv[]) {
-    printf("Hi, I'm ClientReq program!\n");
     printf("Benvenuto! I servizi disponibili sono i seguenti:\n - stampa\n - salva\n - invia");
 
+    // inserimento user_id
     char user_id[50];
     printf("\n\nInserire codice identificativo utente: ");
     scanf("%s", user_id);
 
+    // selezione servizio
     char service[50];
     do {
       printf("Inserire il nome del servizio richiesto: ");
       scanf("%s", service);
-
-      if (strcmp(service, "stampa") != 0 &&
-          strcmp(service, "salva") != 0 &&
-          strcmp(service, "invia") != 0)
-      { printf("\nErr. Il nome va scelto tra \"stampa\" - \"salva\" - \"invia\"\n"); }
     }
-    while (strcmp(service, "stampa") != 0 && strcmp(service, "salva") != 0 && strcmp(service, "invia") != 0);
-    // TO_DO: migliorare questo do - while con una funz che testa la condiz e stampa
-    //        il messaggio d'errore da mettere dentro il while (in modo da eliminare l'if)
+    while (!check_service_input(service));
 
     return 0;
 }
