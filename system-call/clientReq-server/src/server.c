e6f9a008b61236494b086e4f886ccae73e96bac1
AlexPasqua <alexpasquali98@gmail.com>
2019-05-24 23:23:57 +0200

    errExit added - server's sigprocmask impostata

--- system-call/clientReq-server/src/server.c
+++ system-call/clientReq-server/src/server.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include "errExit.h"
 
 int main (int argc, char *argv[]) {
-    printf("Hi, I'm Server program!\n");
-    return 0;
+  // blocco tutti i signal tranne SIGTERM
+  sigset_t signal_set;
+  if (sigfillset(&signal_set) == -1)
+    errExit("sigfillset failed");
+
+  if (sigdelset(&signal_set, SIGTERM) == -1)
+    errExit("sigdelset failed");
+
+  if (sigprocmask(SIG_SETMASK, &signal_set, NULL) == -1)
+    errExit("sigprocmask failed");
+
+  // --> crea FIFOSERVER
+
+
+
+
+
+  // --> elimina FIFOSERVER
+  return 0;
 }
