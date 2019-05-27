5f950bcf14c3a756a9203919080741ad56a3f7ed
AlexPasqua <alexpasquali98@gmail.com>
2019-05-27 18:54:10 +0200

    KEYMANAGER CREATED!

--- system-call/clientReq-server/src/server.c
+++ system-call/clientReq-server/src/server.c
@@ -9,6 +9,7 @@
 #include <sys/sem.h>
 #include <time.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 
 #include "errExit.h"
 #include "myfifo.h"
@@ -33,24 +34,7 @@ int main (int argc, char *argv[]) {
     errExit("sigprocmask failed");
 
 
-  // creo un insieme di semafori per gestire la comunicaz su FIFO
-  key_t sem_key = ftok("src/semaphores.c", 'a');
-  if (sem_key == -1)
-    errExit("Server failed to create a key for the semaphores set");
-
-  int semid = semget(sem_key, 2, IPC_CREAT | /*IPC_EXCL |*/ S_IRUSR | S_IWUSR);
-  if (semid == -1)
-    errExit("Server failed to perform semget");
-
-  unsigned short sem_values[2] = {0, 1}; /* il primo è per il server,
-  il secondo è per mutua esclusione tra i client*/
-  union semun arg;
-  arg.array = sem_values;
-  if (semctl(semid, 0/*ignored*/, SETALL, arg) == -1)
-    errExit("Server failed to set semaphores values");
-
-
-  // creo e "attacco" il segmento di memoria condivisa
+  // creo il segmento di memoria condivisa
   key_t shm_key = ftok("src/server.c", 'a');
   if (shm_key == -1)
     errExit("Server failed to create a key for the shared mem segment");
@@ -59,99 +43,137 @@ int main (int argc, char *argv[]) {
   if (shmid == -1)
     errExit("Server: shmget failed");
 
-  struct Entry *shmptr = (struct Entry *) shmat(shmid, NULL, 0);
-  if (shmptr == (void *)(-1))
-    errExit("Server: shmat failed");
 
 
-  // creo FIFOSERVER
-  char *fifoserv_pathname = "/tmp/FIFOSERVER";
-  char *fifocli_pathname = "/tmp/FIFOCLIENT";
-  if (mkfifo(fifoserv_pathname, S_IRUSR | S_IWUSR) == -1)
-    errExit("mkfifo (FIFOSERVER) failed");
 
 
   // creo KeyManager
-  
+  pid_t km_pid = fork();
+  if (km_pid == -1)
+    errExit("Server: fork() failed");
 
+  else if (km_pid == 0){
+    //----KEY MANAGER SECTION
 
-  // apro FIFOSERVER
-  int fifoserver = open(fifoserv_pathname, O_RDONLY);
-  if (fifoserver == -1)
-    errExit("Server failed to open FIFOSERVER in read-only mode");
+    // "attach" della memoria condivisa
+    struct Entry *shmptr = (struct Entry *) shmat(shmid, NULL, 0);
+    if (shmptr == (void *)(-1))
+      errExit("Server: shmat failed");
 
-
-
-  // continua a controllare richieste dei client
-  struct Request client_data;
-  struct Response resp;
-  int bR, entry_idx = 0;
-  while (1){
-    // blocco il server finché un client non crea FIFOCLIENT
-    semOp(semid, SRVSEM, -1);
-
-    // apro FIFOCLIENT
-    int fifoclient = open(fifocli_pathname, O_WRONLY);
-    if (fifoclient == -1)
-      errExit("Server failed to open FIFOCLIENT in write-only mode");
-
-    // leggo i dati dal server
-    bR = read(fifoserver, &client_data, sizeof(struct Request));
-    if (bR == -1) { errExit("Server failed to perdorm a read from FIFOSERVER"); }
-    else if (bR != sizeof(struct Request)) { errExit("Looks like server didn't received a struct Request correctly"); }
-
-
-    printf("%s - %s, sto generando una chiave di utilizzo...\n", client_data.user, client_data.service);
-
-    /* genero la chiave:
-     *  prendo il timestamp, accodo il numero corrispondente all'iniziale
-     *  dello user, una cifra per il servizio (0=stampa, 1=salva, 2=invia)
-     *  e una cifra casuale. Dopodiché elimino le prime 3 cifre (che sono sempre uguali)
-     *
-     *  per il servizio controllo solo i primi 2 caratteri di service
-     *  (sono già sicuro che le stringhe siano corrette)
-     */
-    unsigned long timestamp = time(NULL);
-    srand(timestamp);
-    resp.key = ((timestamp * 100000) + (client_data.user[0] * 100) +
-               ((client_data.service[0] == 'i') ? 20 : ((client_data.service[1] == 't') ? 0 : 10)) +
-               (rand() % 10)) % THOUSAND_BILLIONS;
-
-    //TO_DO->scrivi su memoria condivisa (occhio ai semafori)
-
-    // rispondo al client
-    if (write(fifoclient, &resp, sizeof(struct Response)) != sizeof(struct Response))
-      errExit("Server failed to write on FIFOCLIENT");
-
-    printf("KEY = %lu\n\n", resp.key);
-
-    // chiudo FIFOCLIENT
-    if (close(fifoclient) == -1)
-      errExit("Server failed to close FIFOCLIENT");
-
-    // sblocco un client in attesa
-    semOp(semid, CLIMUTEX, 1);
   }
+  else{
+    //----PARENT SECTION
+
+    // creo un insieme di semafori per gestire la comunicaz su FIFO
+    key_t sem_key = ftok("src/semaphores.c", 'a');
+    if (sem_key == -1)
+      errExit("Server failed to create a key for the semaphores set");
+
+    int semid = semget(sem_key, 2, IPC_CREAT | S_IRUSR | S_IWUSR);
+    if (semid == -1)
+      errExit("Server failed to perform semget");
+
+    unsigned short sem_values[2] = {0, 1}; /* il primo è per il server,
+    il secondo è per mutua esclusione tra i client*/
+    union semun arg;
+    arg.array = sem_values;
+    if (semctl(semid, 0/*ignored*/, SETALL, arg) == -1)
+      errExit("Server failed to set semaphores values");
+
+
+    // creo e apro FIFOSERVER
+    char *fifoserv_pathname = "/tmp/FIFOSERVER";
+    char *fifocli_pathname = "/tmp/FIFOCLIENT";
+    if (mkfifo(fifoserv_pathname, S_IRUSR | S_IWUSR) == -1)
+      errExit("mkfifo (FIFOSERVER) failed");
+
+    int fifoserver = open(fifoserv_pathname, O_RDONLY);
+    if (fifoserver == -1)
+      errExit("Server failed to open FIFOSERVER in read-only mode");
+
+
+    // "attach" della memoria condivisa
+    struct Entry *shmptr = (struct Entry *) shmat(shmid, NULL, 0);
+    if (shmptr == (void *)(-1))
+      errExit("Server: shmat failed");
 
 
 
-  // chiudo ed elimino FIFOSERVER
-  if (close(fifoserver) == -1)
-    errExit("Server failed to close FIFOSERVER");
+    // continua a controllare richieste dei client
+    struct Request client_data;
+    struct Response resp;
+    int bR, entry_idx = 0;
+    while (1){
+      // blocco il server finché un client non crea FIFOCLIENT
+      semOp(semid, SRVSEM, -1);
 
-  if (unlink(fifoserv_pathname) != 0)
-    errExit("Server failed to unlink FIFOSERVER");
+      // apro FIFOCLIENT
+      int fifoclient = open(fifocli_pathname, O_WRONLY);
+      if (fifoclient == -1)
+        errExit("Server failed to open FIFOCLIENT in write-only mode");
 
-  // elimino il set di semafori per le FIFO
-  if (semctl(semid, 0/*ignored*/, IPC_RMID, NULL) == -1)
-    errExit("Server failed to remove semaphores set");
+      // leggo i dati dal server
+      bR = read(fifoserver, &client_data, sizeof(struct Request));
+      if (bR == -1) { errExit("Server failed to perdorm a read from FIFOSERVER"); }
+      else if (bR != sizeof(struct Request)) { errExit("Looks like server didn't received a struct Request correctly"); }
 
-  // detach & delete memoria condivisa
-  if (shmdt(shmptr) != 0)
-    errExit("Server: shmdt failed");
 
-  if (shmctl(shmid, IPC_RMID, NULL) != 0)
-    errExit("Server failed to delete shared memory segment");
+      printf("%s - %s, sto generando una chiave di utilizzo...\n", client_data.user, client_data.service);
+
+      /* genero la chiave:
+       *  prendo il timestamp, accodo il numero corrispondente all'iniziale
+       *  dello user, una cifra per il servizio (0=stampa, 1=salva, 2=invia)
+       *  e una cifra casuale. Dopodiché elimino le prime 3 cifre (che sono sempre uguali)
+       *
+       *  per il servizio controllo solo i primi 2 caratteri di service
+       *  (sono già sicuro che le stringhe siano corrette)
+       */
+      unsigned long timestamp = time(NULL);
+      srand(timestamp);
+      resp.key = ((timestamp * 100000) + (client_data.user[0] * 100) +
+                 ((client_data.service[0] == 'i') ? 20 : ((client_data.service[1] == 't') ? 0 : 10)) +
+                 (rand() % 10)) % THOUSAND_BILLIONS;
+
+      //TO_DO->scrivi su memoria condivisa (occhio ai semafori)
+
+      // rispondo al client
+      if (write(fifoclient, &resp, sizeof(struct Response)) != sizeof(struct Response))
+        errExit("Server failed to write on FIFOCLIENT");
+
+      printf("KEY = %lu\n\n", resp.key);
+
+      // chiudo FIFOCLIENT
+      if (close(fifoclient) == -1)
+        errExit("Server failed to close FIFOCLIENT");
+
+      // sblocco un client in attesa
+      semOp(semid, CLIMUTEX, 1);
+    }
+
+
+
+    // aspetto la terminazione di KeyManager per non farlo diverntare orfano
+    if (waitpid(km_pid, NULL, 0) == -1)
+      errExit("Server: waitpid() failed");
+
+    // chiudo ed elimino FIFOSERVER
+    if (close(fifoserver) == -1)
+      errExit("Server failed to close FIFOSERVER");
+
+    if (unlink(fifoserv_pathname) != 0)
+      errExit("Server failed to unlink FIFOSERVER");
+
+    // elimino il set di semafori per le FIFO
+    if (semctl(semid, 0/*ignored*/, IPC_RMID, NULL) == -1)
+      errExit("Server failed to remove semaphores set");
+
+    // detach & delete memoria condivisa
+    if (shmdt(shmptr) != 0)
+      errExit("Server: shmdt failed");
+
+    if (shmctl(shmid, IPC_RMID, NULL) != 0)
+      errExit("Server failed to delete shared memory segment");
+  }
 
 
   return 0;
