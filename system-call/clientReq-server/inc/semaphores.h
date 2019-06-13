cbee6d30eaf361a0004f2b8ac0febefcc0ad11c2
AlexPasqua <alexpasquali98@gmail.com>
2019-06-13 10:20:50 +0200

    func 'crt_semaphores' imbastita -> RCD

--- system-call/clientReq-server/inc/semaphores.h
+++ system-call/clientReq-server/inc/semaphores.h
@@ -3,6 +3,8 @@
 
 #define SRVSEM 0
 #define CLIMUTEX 1
+#define SEMTYPE_FIFO 0
+#define SEMTYPE_SHM 1
 
 // definizione della union semun
 union semun {
@@ -11,6 +13,8 @@ union semun {
   unsigned short *array;
 };
 
+// dichiarazione funcioni
+void crt_semaphores(short type, int *semid);  //crea un semaphores set per le fifo o shm
 void semOp (int semid, unsigned short sem_num, short sem_op);
 
 #endif
--- system-call/clientReq-server/src/semaphores.c
+++ system-call/clientReq-server/src/semaphores.c
@@ -4,6 +4,20 @@
 #include "semaphore.h"
 #include "../inc/errExit.h"
 
+// implementazione funzioni ----------------------------------------------------
+void crt_semaphores(short type, int *semid){
+  switch (type){
+    case SEMTYPE_FIFO: break;
+
+
+    case SEMTYPE_SHM: break;
+
+
+    default: break;
+  }
+}
+
+//------------------------------------------------------------------------------
 void semOp (int semid, unsigned short sem_num, short sem_op) {
   struct sembuf sop = {.sem_num = sem_num, .sem_op = sem_op, .sem_flg = 0};
 
