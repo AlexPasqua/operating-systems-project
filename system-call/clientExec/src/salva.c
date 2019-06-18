bbaacdd90a664f20ea348b3aba3b53d000aa3430
ALEX PASQUALI <alexpasquali98@gmail.com>
2019-06-18 18:35:03 +0200

    'salva' fixed

--- system-call/clientExec/src/salva.c
+++ system-call/clientExec/src/salva.c
@@ -11,16 +11,20 @@ int main (int argc, char *argv[]) {
   if (argc < 2)
     errExit("Usage: ./salva <file name> <args>");
 
-  int fd = open(argv[1],  O_CREAT | O_TRUNC | O_RDWR);
+  int fd = open(argv[1],  O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
   if (fd == -1)
     errExit("salva: failed to open the file");
 
+  char result[200];
+  sprintf(result, "%s", "");
   for (int i = 2; i < argc; i++){
-    int size = 1;
-    for ( ; argv[i][size - 1] != '\0'; size++); //conto la lunghezza della stringa
-    if (write(fd, argv[i], size) != size)
-      errExit("salva: write on file failed");
+    strcat(result, argv[i]);
+    strcat(result, " ");
   }
+
+  unsigned int size = strlen(result);
+  if (write(fd, result, size) != size)
+        errExit("salva: write on file failed");
 
   return 0;
 }
